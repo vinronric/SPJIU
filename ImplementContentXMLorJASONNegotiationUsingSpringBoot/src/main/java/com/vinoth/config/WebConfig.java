2e3930955070fee866ebf35f0fbd0f73ac408c43
vinronric <vinronric@gmail.com>
2019-04-15 12:48:54 -0400

    WebMvcConfigurer interface is used instead of WebMvcConfigurerAdapter class since it is deprecated

--- ImplementContentXMLorJASONNegotiationUsingSpringBoot/src/main/java/com/vinoth/config/WebConfig.java
+++ ImplementContentXMLorJASONNegotiationUsingSpringBoot/src/main/java/com/vinoth/config/WebConfig.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer  {
 
 	// configureContentNegotiation with Path Extension [e.g  localhost:8081/employee.json (or) localhost:8081/employee.xml ]
 	/*@Override
